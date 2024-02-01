4ecb3a337044b66c464ba7ac478f37ea10397061
antho <anthonyyoussef01@gmail.com>
2024-02-01 03:30:29 -0500

    trying to find a fix for the arabic text not aligning to the right (everything is aligned to justify rn)... gonna leave it and TODO: will need to look at it again some other time!

--- src/components/pages/WordOfTheDay.tsx
+++ src/components/pages/WordOfTheDay.tsx
@@ -4,16 +4,16 @@ import './WordOfTheDay.scss';
 const WordOfTheDay: React.FC = () => {
     return (
         <div className={"word-of-day"}>
-            <p className={"word-of-day-word"} dir="rtl">
+            <p className={"word-of-day-word"} dir={"rtl"} lang={"ar"}>
                 النهاردة
             </p>
             <p className={"word-of-day-definition"}>
-                <span className={"word-of-day-definition-ar"} dir="rtl">
+                <span className={"word-of-day-definition-ar"} dir={"rtl"} lang={"ar"}>
                     «النهاردة» هي كلمة في اللهجة المصرية تعبر عن اليوم الحالي أو الفترة الزمنية التي نعيشها حاليًا.
                     يُستخدم مصطلح «النهاردة» للإشارة إلى الزمن الحالي بشكل عام.
                 </span>
                 <br/><br/>
-                <span className={"word-of-day-definition-en"}>
+                <span className={"word-of-day-definition-en"} lang={"en"}>
                     The Arabic word "النهاردة" (pronounced: al-naharda) translates to "today" in English. It is commonly
                     used in various Arabic dialects, including the Egyptian dialect. This term is employed to refer to the
                     current day or the present moment, indicating the temporal context of events or discussions. In English,
