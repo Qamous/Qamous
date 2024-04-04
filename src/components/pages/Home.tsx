6ec0a8c8990e95c7766fe1600a42437a55c5162f
antho <anthonyyoussef01@gmail.com>
2024-04-04 18:36:17 -0400

    API Testing:
    - Added api testing in the frontend

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.scss';
 import { useTranslation } from 'react-i18next';
 import i18n from 'i18next';
@@ -16,6 +16,15 @@ const Home: React.FC = () => {
     }) as HomeContent[];
     // get language
     const lang = i18n.language;
+    // get words from api
+    useEffect(() => {
+        fetch('http://localhost:3000/word/search/all', {
+            mode: 'cors',
+            credentials: 'include'
+        })
+          .then(response => console.log(response.json()))
+          .catch(error => console.error('Error:', error))
+    }, []);
 
     return (
         <div className={"home"}>
