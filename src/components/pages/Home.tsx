b831f8f43f04bca251ade56bed933f8621dd1ed7
antho <anthonyyoussef01@gmail.com>
2024-04-09 13:20:05 -0400

    Home Page Updates: - Calling the api from the backend for the home page (lang stuff doesn't work)

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.scss';
 import { useTranslation } from 'react-i18next';
 import i18n from 'i18next';
 import ContentBox from '../ContentBox';
 
 interface HomeContent {
+    word: string, // Add this line
+    definition: string,
+    likeCount: number,
+    dislikeCount: number,
+    likeDislikeDifference: number,
+    isArabic: boolean,
+    arabicWord: string,
+    reportCount: number
+}
+
+interface JsonContent {
     word: string,
     definition: string
 }
@@ -13,22 +24,23 @@ const Home: React.FC = () => {
     const { t } = useTranslation();
     const sampleHome = t('sample_home', {
         returnObjects: true
-    }) as HomeContent[];
-    // get language
+    }) as JsonContent[];
+    const [homeContent, setHomeContent] = useState<JsonContent[]>(sampleHome);
     const lang = i18n.language;
-    // get words from api
+
     useEffect(() => {
-        fetch('http://localhost:3000/word/search/all', {
+        fetch('http://localhost:3000/definitions/most-liked', {
             mode: 'cors',
             credentials: 'include'
         })
-          .then(response => console.log(response.json()))
-          .catch(error => console.error('Error:', error))
+        .then(response => response.json())
+        .then(data => setHomeContent([sampleHome[0], ...data]))
+        .catch(error => console.error('Error:', error));
     }, []);
-
+    console.log(homeContent);
     return (
         <div className={"home"}>
-            {sampleHome.map((item, index) => (
+            {homeContent.map((item, index) => (
                 <ContentBox
                   key={index}
                   item={item}
