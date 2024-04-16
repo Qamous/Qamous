3f1e0460f437308d19feef337250cdaac4c810d3
antho <anthonyyoussef01@gmail.com>
2024-04-16 12:48:59 -0400

    Word of The Day Page Updates:
    - Word of the day now pulls from the most liked definitions api and selects a random word
    - Made sure arabic and english definitions belong to the same word
    - language control does not affect / should not affect page
    - changed the overflow to visible for the word-of-day-word because it was affecting how yalla was showing on the word of the day page

--- src/components/pages/WordOfTheDay.tsx
+++ src/components/pages/WordOfTheDay.tsx
@@ -1,27 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './WordOfTheDay.scss';
 
+interface WordOfTheDayContent {
+    wordId: number,
+    word: string,
+    definition: string,
+    likeCount: number,
+    dislikeCount: number,
+    likeDislikeDifference: number,
+    isArabic: number,
+    francoArabicWord: string,
+    reportCount: number
+}
+
 const WordOfTheDay: React.FC = () => {
+    const [arabicWordOfTheDay, setArabicWordOfTheDay] = useState<WordOfTheDayContent | null>(null);
+    const [englishWordOfTheDay, setEnglishWordOfTheDay] = useState<WordOfTheDayContent | null>(null);
+
+    useEffect(() => {
+    fetch('http://localhost:3000/definitions/most-liked', {
+        mode: 'cors',
+        credentials: 'include',
+    })
+        .then(response => response.json())
+        .then(data => {
+            //randomize data order
+            data.sort(() => Math.random() - 0.5);
+            // Find the first Arabic word
+            const arabicWord = data.find((word: WordOfTheDayContent) => word.isArabic === 1);
+
+            // Find the English word that has the same definition as the Arabic word
+            const englishWord = data.find((word: WordOfTheDayContent) => word.isArabic === 0 && word.wordId === arabicWord.wordId);
+
+            setArabicWordOfTheDay(arabicWord);
+            setEnglishWordOfTheDay(englishWord);
+        })
+        .catch(error => console.error('Error:', error));
+}, []);
+
     return (
-        <div className={"word-of-day"}>
-            <p className={"word-of-day-word"} dir={"rtl"} lang={"ar"}>
-                النهاردة
-            </p>
-            <div className={"word-of-day-definition"}>
-                <p className={"word-of-day-definition-ar"} dir={"rtl"} lang={"ar"}>
-                    «النهاردة» هي كلمة في اللهجة المصرية تعبر عن اليوم الحالي أو الفترة الزمنية التي نعيشها حاليًا.
-                    يُستخدم مصطلح «النهاردة» للإشارة إلى الزمن الحالي بشكل عام.
-                </p>
-                <p className={"word-of-day-definition-en"} lang={"en"}>
-                    The Arabic word "النهاردة" (pronounced: al-naharda) translates to "today" in English. It is commonly
-                    used in various Arabic dialects, including the Egyptian dialect. This term is employed to refer to the
-                    current day or the present moment, indicating the temporal context of events or discussions. In English,
-                    it is synonymous with "today," representing the immediate time frame in which a conversation or
-                    situation is taking place.
-                </p>
-            </div>
+        <div className={'word-of-day'}>
+            {arabicWordOfTheDay && englishWordOfTheDay && (
+                <>
+                    <p className={'word-of-day-word'} dir={'rtl'} lang={'ar'}>
+                        {arabicWordOfTheDay.word}
+                    </p>
+                    <div className={'word-of-day-definition'}>
+                        <p className={'word-of-day-definition-ar'} dir={'rtl'} lang={'ar'}>
+                            {arabicWordOfTheDay.definition}
+                        </p>
+                        <p className={'word-of-day-definition-en'} lang={'en'}>
+                            {englishWordOfTheDay.definition}
+                        </p>
+                    </div>
+                </>
+            )}
         </div>
     );
-}
+};
 
 export default WordOfTheDay;
