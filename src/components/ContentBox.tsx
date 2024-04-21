c54b548af7250f927c8d8adb3861e0b0f29f8831
antho <anthonyyoussef01@gmail.com>
2024-04-21 14:48:16 -0400

    Countries UI Updates:
    - Added the countries' UI for words

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -2,134 +2,163 @@ import React, { useEffect, useState } from 'react';
 import './ContentBox.scss';
 import Snackbar from './Snackbar';
 import { useTranslation } from 'react-i18next';
+import { useQuery } from 'react-query';
+import { Country } from '../../backend/src/typeorm/entities/country';
+import ReactCountryFlag from 'react-country-flag';
 
 interface HomeContentProps {
-    item: {
-        word: string,
-        definition: string
-    },
-    index: number,
-    lang: string
+  item: {
+    word: string,
+    definition: string
+  },
+  index: number,
+  lang: string,
+  definitionId: number,
+  //likeCount: number,
+  //dislikeCount: number,
 }
 
 interface ButtonText {
-    like: string,
-    dislike: string,
-    report: string
+  like: string,
+  dislike: string,
+  report: string
 }
 
-const ContentBox: React.FC<HomeContentProps> = ({ item, index, lang }) => {
-    const { t } = useTranslation();
-    const buttonText = t('content_box_buttons', {
-        returnObjects: true
-    }) as ButtonText;
+const fetchCountry = async (definitionId: number): Promise<Country> => {
+  const response = await fetch(`http://localhost:3000/definitions/${definitionId}/country`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
 
-    const [likeClicked, setLikeClicked] = useState(false);
-    const [dislikeClicked, setDislikeClicked] = useState(false);
-    const [reportClicked, setReportClicked] = useState(false);
-    const [reportSnackbarOpen, setReportSnackbarOpen] = useState(false);
-    const [clickCount, setClickCount] = useState(0);
-    const [excessiveClickSnackbarOpen, setExcessiveClickSnackbarOpen] = useState(false);
-
-    const handleLikeClick = () => {
-        if (clickCount < 5) {
-            setClickCount(prevCount => prevCount + 1);
-            setLikeClicked(!likeClicked);
-            if (dislikeClicked) setDislikeClicked(false);
+const ContentBox: React.FC<HomeContentProps> = ({ item, index, lang, definitionId }) => {
+  const { t } = useTranslation();
+  const buttonText = t('content_box_buttons', {
+    returnObjects: true,
+  }) as ButtonText;
+  
+  const [likeClicked, setLikeClicked] = useState(false);
+  const [dislikeClicked, setDislikeClicked] = useState(false);
+  const [reportClicked, setReportClicked] = useState(false);
+  const [reportSnackbarOpen, setReportSnackbarOpen] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
+  const [excessiveClickSnackbarOpen, setExcessiveClickSnackbarOpen] = useState(false);
+  const { data: country, isError, isLoading } =
+    useQuery(['country', definitionId], () => fetchCountry(definitionId), {
+      enabled: definitionId !== 0,
+    });
+  
+  const handleLikeClick = () => {
+    if (clickCount < 5) {
+      setClickCount(prevCount => prevCount + 1);
+      setLikeClicked(!likeClicked);
+      if (dislikeClicked) setDislikeClicked(false);
+    }
+    if (clickCount >= 5) {
+      setExcessiveClickSnackbarOpen(true);
+      setTimeout(() => setExcessiveClickSnackbarOpen(false), 3000);
+    }
+  };
+  
+  const handleDislikeClick = () => {
+    if (clickCount < 5) {
+      setClickCount(prevCount => prevCount + 1);
+      setDislikeClicked(!dislikeClicked);
+      if (likeClicked) setLikeClicked(false);
+    }
+    if (clickCount >= 5) {
+      setExcessiveClickSnackbarOpen(true);
+      setTimeout(() => setExcessiveClickSnackbarOpen(false), 3000);
+    }
+  };
+  
+  const handleReportClick = () => {
+    if (reportClicked) {
+      setReportSnackbarOpen(true);
+      setTimeout(() => setReportSnackbarOpen(false), 3000);
+    } else {
+      const reportType = window.prompt('Are you reporting the word or the definition? Please enter \'word\' or \'definition\':');
+      if (reportType === null) {
+        window.alert('Report canceled.');
+      } else if (reportType === 'word' || reportType === 'definition') {
+        const reason = window.prompt(`Please enter the reason for reporting this ${reportType}:`);
+        if (reason || reason === '') {
+          // TODO: use the 'reason' and 'reportType' variables here to send them to the server
+          console.log(`Report Type: ${reportType}, Reason: ${reason}`);
+          setReportClicked(true);
+        } else if (reason === null) {
+          window.alert('Report canceled.');
         }
-        if (clickCount >= 5) {
-            setExcessiveClickSnackbarOpen(true);
-            setTimeout(() => setExcessiveClickSnackbarOpen(false), 3000);
+      } else {
+        window.alert('Invalid input. Please enter \'word\' or \'definition\'.');
+      }
+    }
+  };
+  
+  useEffect(() => {
+    if (clickCount >= 5) {
+      setTimeout(() => {
+        setClickCount(0);
+      }, 60000); // Reset after 1 minute
+    }
+  }, [clickCount]);
+  
+  return (
+    <div className={'content-box' +
+      (index === 0 ? ' content-box-first' : '') +
+      (lang === 'ar' ? ' content-box-ar' : ' content-box-latin')}>
+      <div className={'content-box-title'}>
+        <h1>{item.word}</h1>
+        {!isLoading && !isError && country &&
+          <ReactCountryFlag
+            countryCode={country.countryCode}
+            svg
+            style={{
+              width: '2em',
+              height: '2em',
+            }}
+            title={country.countryName}
+          />
         }
-    };
-
-    const handleDislikeClick = () => {
-        if (clickCount < 5) {
-            setClickCount(prevCount => prevCount + 1);
-            setDislikeClicked(!dislikeClicked);
-            if (likeClicked) setLikeClicked(false);
-        }
-        if (clickCount >= 5) {
-            setExcessiveClickSnackbarOpen(true);
-            setTimeout(() => setExcessiveClickSnackbarOpen(false), 3000);
-        }
-    };
-
-    const handleReportClick = () => {
-        if (reportClicked) {
-            setReportSnackbarOpen(true);
-            setTimeout(() => setReportSnackbarOpen(false), 3000);
-        } else {
-            const reportType = window.prompt("Are you reporting the word or the definition? Please enter 'word' or 'definition':");
-            if (reportType === null) {
-                window.alert("Report canceled.");
-            } else if (reportType === 'word' || reportType === 'definition') {
-                const reason = window.prompt(`Please enter the reason for reporting this ${reportType}:`);
-                if (reason || reason === '') {
-                    // TODO: use the 'reason' and 'reportType' variables here to send them to the server
-                    console.log(`Report Type: ${reportType}, Reason: ${reason}`);
-                    setReportClicked(true);
-                } else if (reason === null) {
-                    window.alert("Report canceled.");
-                }
-            } else {
-                window.alert("Invalid input. Please enter 'word' or 'definition'.");
-            }
-        }
-    };
-
-    useEffect(() => {
-        if (clickCount >= 5) {
-            setTimeout(() => {
-                setClickCount(0);
-            }, 60000); // Reset after 1 minute
-        }
-    }, [clickCount]);
-
-    return (
-        <div className={"content-box" +
-            (index === 0 ? " content-box-first" : "") +
-            (lang === 'ar' ? " content-box-ar" : " content-box-latin")}>
-            <div className={"content-box-title"}>
-                <h1>{item.word}</h1>
-            </div>
-            <div className={"content-box-description"}>
-                <p>
-                    {item.definition}
-                </p>
-            </div>
-            {index !== 0 && (
-              <div className="content-box-buttons">
-                  <button
-                    className={`content-box-buttons-like-button ${likeClicked ? 'clicked' : ''}`}
-                    onClick={handleLikeClick}
-                  >
-                      {buttonText.like}
-                  </button>
-                  <button
-                    className={`content-box-buttons-dislike-button ${dislikeClicked ? 'clicked' : ''}`}
-                    onClick={handleDislikeClick}
-                  >
-                      {buttonText.dislike}
-                  </button>
-                  <button
-                    className={`content-box-buttons-report-button ${reportClicked ? 'clicked' : ''}`}
-                    onClick={handleReportClick}
-                  >
-                      {buttonText.report}
-                  </button>
-              </div>
-            )}
-            <Snackbar
-              open={reportSnackbarOpen}
-              message="You are unable to report the same word more than once"
-            />
-            <Snackbar
-              open={excessiveClickSnackbarOpen}
-              message="You have reacted too many times. Please wait before clicking again."
-            />
+      </div>
+      <div className={'content-box-description'}>
+        <p>
+          {item.definition}
+        </p>
+      </div>
+      {index !== 0 && (
+        <div className="content-box-buttons">
+          <button
+            className={`content-box-buttons-like-button ${likeClicked ? 'clicked' : ''}`}
+            onClick={handleLikeClick}
+          >
+            {buttonText.like}
+          </button>
+          <button
+            className={`content-box-buttons-dislike-button ${dislikeClicked ? 'clicked' : ''}`}
+            onClick={handleDislikeClick}
+          >
+            {buttonText.dislike}
+          </button>
+          <button
+            className={`content-box-buttons-report-button ${reportClicked ? 'clicked' : ''}`}
+            onClick={handleReportClick}
+          >
+            {buttonText.report}
+          </button>
         </div>
-    );
-}
+      )}
+      <Snackbar
+        open={reportSnackbarOpen}
+        message="You are unable to report the same word more than once"
+      />
+      <Snackbar
+        open={excessiveClickSnackbarOpen}
+        message="You have reacted too many times. Please wait before clicking again."
+      />
+    </div>
+  );
+};
 
 export default ContentBox;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -7,98 +7,102 @@ import { useQuery } from 'react-query';
 import Snackbar from '../Snackbar';
 
 interface HomeContent {
-    word: string,
-    definition: string,
-    likeCount: number,
-    dislikeCount: number,
-    likeDislikeDifference: number,
-    isArabic: number,
-    francoArabicWord: string,
-    reportCount: number
+  word: string,
+  definition: string,
+  definitionId: number,
+  likeCount: number,
+  dislikeCount: number,
+  likeDislikeDifference: number,
+  isArabic: number,
+  francoArabicWord: string,
+  reportCount: number
 }
 
 interface JsonContent {
-    word: string,
-    definition: string
+  word: string,
+  definition: string
 }
 
-const fetchHomeContent = () =>
-    fetch('http://localhost:3000/definitions/most-liked', {
-        mode: 'cors',
-        credentials: 'include',
-    })
-        .then(response => response.json());
+const fetchHomeContent: () => Promise<HomeContent[]> = () =>
+  fetch('http://localhost:3000/definitions/most-liked', {
+    mode: 'cors',
+    credentials: 'include',
+  }).then(response => response.json());
 
 const Home: React.FC = () => {
-    const { t } = useTranslation();
-    const sampleHome = t('sample_home', {
-        returnObjects: true,
-    }) as JsonContent[];
-    const lang = i18n.language;
-
-    const { data: homeContent, isLoading, isError } = useQuery<HomeContent[]>('homeContent', fetchHomeContent);
-    const [errorSnackbarOpen, setErrorSnackbarOpen] = useState(false);
-
-    useEffect(() => {
-        if (isError) {
-            setErrorSnackbarOpen(true);
-            setTimeout(() => setErrorSnackbarOpen(false), 3000);
-        }
-    }, [isError]);
-
-    if (isLoading) {
-        return (
-            <div className={'home'}>
-                <ContentBox
-                    item={sampleHome[0]}
-                    index={0}
-                    lang={lang}
-                />
-                <div className={'loading-ring'}>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
-        );
-    }
-
+  const { t } = useTranslation();
+  const sampleHome = t('sample_home', {
+    returnObjects: true,
+  }) as JsonContent[];
+  const lang = i18n.language;
+  
+  const { data: homeContent, isLoading, isError } = useQuery<HomeContent[]>('homeContent', fetchHomeContent);
+  const [errorSnackbarOpen, setErrorSnackbarOpen] = useState(false);
+  
+  useEffect(() => {
     if (isError) {
-        return (
-            <div className={'home'}>
-                <ContentBox
-                    item={sampleHome[0]}
-                    index={0}
-                    lang={lang}
-                />
-                <Snackbar
-                    open={errorSnackbarOpen}
-                    message={'Data fetching error occurred. Please try again later.'}
-                />
-            </div>
-        );
+      setErrorSnackbarOpen(true);
+      setTimeout(() => setErrorSnackbarOpen(false), 3000);
     }
-
+  }, [isError]);
+  
+  if (isLoading) {
     return (
-        <div className={'home'}>
-            <ContentBox
-                item={sampleHome[0]}
-                index={0}
-                lang={lang}
-            />
-            {homeContent && homeContent
-                .filter(item => item.isArabic === (lang === 'ar' ? 1 : 0))
-                .map((item, index) => (
-                    <ContentBox
-                        key={index + 1}
-                        item={item}
-                        index={index + 1}
-                        lang={lang}
-                    />
-                ))}
+      <div className={'home'}>
+        <ContentBox
+          item={sampleHome[0]}
+          index={0}
+          lang={lang}
+          definitionId={0}
+        />
+        <div className={'loading-ring'}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
+      </div>
     );
+  }
+  
+  if (isError) {
+    return (
+      <div className={'home'}>
+        <ContentBox
+          item={sampleHome[0]}
+          index={0}
+          lang={lang}
+          definitionId={0}
+        />
+        <Snackbar
+          open={errorSnackbarOpen}
+          message={'Data fetching error occurred. Please try again later.'}
+        />
+      </div>
+    );
+  }
+  
+  return (
+    <div className={'home'}>
+      <ContentBox
+        item={sampleHome[0]}
+        index={0}
+        lang={lang}
+        definitionId={0}
+      />
+      {homeContent && homeContent
+        .filter(item => item.isArabic === (lang === 'ar' ? 1 : 0))
+        .map((item, index) => (
+          <ContentBox
+            key={index + 1}
+            item={item}
+            index={index + 1}
+            lang={lang}
+            definitionId={item.definitionId}
+          />
+        ))}
+    </div>
+  );
 };
 
 export default Home;
