7c1788097dfd56b8bbca395a5d957db8576227b6
antho <anthonyyoussef01@gmail.com>
2024-05-16 21:48:02 -0400

    CustomDialog Updates:
    - Updated the x button to work

--- src/components/CustomDialog.tsx
+++ src/components/CustomDialog.tsx
@@ -9,13 +9,14 @@ interface CustomDialogProps {
   buttonText2: string;
   onButton1Click: () => void;
   onButton2Click: () => void;
+  onClose: () => void;
 }
 
-const CustomDialog: React.FC<CustomDialogProps> = ({ text, buttonText1, buttonText2, onButton1Click, onButton2Click }) => {
+const CustomDialog: React.FC<CustomDialogProps> = ({ text, buttonText1, buttonText2, onButton1Click, onButton2Click, onClose }) => {
   return (
     <div className="custom-dialog">
       <div className="custom-dialog-content">
-        <FontAwesomeIcon className="custom-dialog-close" onClick={onButton1Click} icon={faX} />
+        <FontAwesomeIcon className="custom-dialog-close" onClick={onClose} icon={faX} />
         <p>{text}</p>
         <button onClick={onButton1Click}>{buttonText1}</button>
         <button onClick={onButton2Click}>{buttonText2}</button>
