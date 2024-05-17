958a0ba4cdc522d6c2da72965a310009f077f29d
antho <anthonyyoussef01@gmail.com>
2024-05-16 22:01:04 -0400

    CustomDialog Updates:
    - Added the option to enter text

--- src/components/CustomDialog.tsx
+++ src/components/CustomDialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CustomDialog.scss';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,16 +10,41 @@ interface CustomDialogProps {
   onButton1Click: () => void;
   onButton2Click: () => void;
   onClose: () => void;
+  showTextInput: boolean;
+  onSubmit: (input: string) => void;
+  onCancel: () => void;
 }
 
-const CustomDialog: React.FC<CustomDialogProps> = ({ text, buttonText1, buttonText2, onButton1Click, onButton2Click, onClose }) => {
+const CustomDialog: React.FC<CustomDialogProps> = ({ text, buttonText1, buttonText2, onButton1Click, onButton2Click, onClose, showTextInput, onSubmit, onCancel }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    onSubmit(inputValue);
+    setInputValue('');
+  };
+
   return (
     <div className="custom-dialog">
       <div className="custom-dialog-content">
         <FontAwesomeIcon className="custom-dialog-close" onClick={onClose} icon={faX} />
         <p>{text}</p>
-        <button onClick={onButton1Click}>{buttonText1}</button>
-        <button onClick={onButton2Click}>{buttonText2}</button>
+        {showTextInput && (
+          <div>
+            <input type="text" value={inputValue} onChange={handleInputChange} />
+            <button onClick={handleSubmit}>Submit</button>
+            <button onClick={onCancel}>Cancel</button>
+          </div>
+        )}
+        {!showTextInput && (
+          <div>
+            <button onClick={onButton1Click}>{buttonText1}</button>
+            <button onClick={onButton2Click}>{buttonText2}</button>
+          </div>
+        )}
       </div>
     </div>
   );
