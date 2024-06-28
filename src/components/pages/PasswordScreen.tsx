a6367a09e9eb69d26f9bfb673050eae0bdb1fca1
antho <anthonyyoussef01@gmail.com>
2024-06-28 15:07:40 -0400

    Entry Password Screen Update:
    - Added caps lock on warning

--- src/components/pages/PasswordScreen.tsx
+++ src/components/pages/PasswordScreen.tsx
@@ -4,7 +4,14 @@ import './PasswordScreen.scss';
 const PasswordScreen: React.FC<{ onPasswordCorrect: () => void }> = ({ onPasswordCorrect }) => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
-
+  const [isCaps, setIsCaps] = useState(false);
+  
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+    setIsCaps(newPassword.toUpperCase() === newPassword && newPassword !== '');
+  };
+  
   const handlePasswordSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     if (password === 'QAMOOSE-THE-GOOSE') {
@@ -19,11 +26,16 @@ const PasswordScreen: React.FC<{ onPasswordCorrect: () => void }> = ({ onPasswor
       <form onSubmit={handlePasswordSubmit}>
         <label>
           Enter password: &nbsp;
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}
-                 className="password-screen-input" />
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            className="password-screen-input"
+          />
         </label>
         <input type="submit" value="Submit" className="password-screen-submit" />
       </form>
+      {isCaps && <p className="password-screen-warning">Warning: Caps Lock is on!</p>}
       {error && <p className="password-screen-error">Incorrect password. Please try again.</p>}
     </div>
   );
